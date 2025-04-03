feef6450f20c8d65d2bd7776bbbec410db084d09
mchost252 <157480985+mchost252@users.noreply.github.com>
2025-04-03 09:29:09 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'PJ Creativity', // Your name
-  ownerNumber: '+234 814 948 4496', //Your number
+  ownerNumber: '+2348149484496', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
